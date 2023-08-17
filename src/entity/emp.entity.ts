ebdc438a7d1af96e9dc6ceb075b6d57cf65a52de
GeoJosh004 <michael@abshrms.com>
2023-08-17 12:21:34 +0530

    Revert "testing"
    This reverts commit dc3e8705b0b516980e038786feff6f333f102831.

--- src/entity/emp.entity.ts
+++ src/entity/emp.entity.ts
@@ -1,5 +1,5 @@
 import { InferAttributes, InferCreationAttributes } from "sequelize";
-import {  Column, HasMany, Model, Table } from "sequelize-typescript";
+import { BelongsTo, Column, HasMany, Model, Table } from "sequelize-typescript";
 import { EDTS } from "./emp_details.entity";
 import { EODE } from "./emp_off_details.entity";
 import { OneToOne } from "typeorm";
@@ -8,18 +8,17 @@ import { OneToOne } from "typeorm";
 export class EETS extends Model<InferCreationAttributes<EETS>, InferAttributes<EETS>>{
 
 
-    @HasMany(() => EDTS, { as: 'empdetail' })
-    empDetail: EDTS[];
-
-    @HasMany(() => EODE, { as: 'empoffdetail' })  
-    empOff: EODE[]; 
-
     @Column({ autoIncrement: true, primaryKey: true })
     id: number;
 
+    @HasMany(() => EDTS, { as: 'empdetail' })
+    empDetail: EDTS[];
+
+    @HasMany(() => EODE, { as: 'empoffdetail' })
+    empOff: EODE[];
 
   
-    @Column({ primaryKey: true})
+    @Column({ primaryKey: true })
     emp_code: string;
 
     @Column
